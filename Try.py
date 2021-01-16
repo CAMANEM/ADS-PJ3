acaeb06ef7c3ddac8ec214efee702363277e7b26
Fernando <fercoche369@gmail.com>
2021-01-15 22:12:02 -0600

    Dijkstra Fix

--- Try.py
+++ Try.py
@@ -4,3 +4,4 @@ G = nx.Graph()
 e = [("A", "B", 1), ("B", "C", 2), ("A", "C", 3), ("C", "D", 4), ("D", "E", 2), ("B", "E", 1)]
 G.add_weighted_edges_from(e)
 print(nx.dijkstra_path(G, "A", "D"))
+print(nx.dag_longest_path(nx.to_directed(G), "A", "D"))
--- graph.py
+++ graph.py
@@ -15,7 +15,7 @@ class Graph:
     def __init__(self):
         self.apex = {}
 
-    def add_apex (self, id):
+    def add_apex(self, id):
         if id not in self.apex:
             self.apex[id] = Vertex(id)
 
@@ -32,7 +32,7 @@ class Graph:
         path = []
         actual = b
         while actual is not None:
-            path.insert(0, actual)
+            path.insert(0, actual) #a por 0
             actual = self.apex[actual].dad
         return [path, self.apex[b].distance]
 
@@ -46,8 +46,19 @@ class Graph:
                     v = i
             return v
 
+    def maximum(self, array):
+        if len(array) > 0:
+            m = self.apex[array[0]].distance
+            v = array[0]
+            for i in array:
+                if m < self.apex[i].distance:
+                    m = self.apex[i].distance
+                    v = i
+            return v
+
     def dijkstra(self, a):
         if a in self.apex:
+
             self.apex[a].distance = 0
             actual = a
             not_visited = []
@@ -61,42 +72,60 @@ class Graph:
             while len(not_visited) > 0:
                 for i in self.apex[actual].neighbor:
                     if not self.apex[i[0]].visited:
-                        if (self.apex[actual].distance + i[1]) < self.apex[i[0]].distance:
+                        if (self.apex[actual].distance + i[1]) < self.apex[i[0]].distance: # el signo original es "<"
                             self.apex[i[0]].distance = (self.apex[actual].distance + i[1])
                             self.apex[i[0]].dad = actual
                 self.apex[actual].visited = True
                 not_visited.remove(actual)
-                actual = self.minimum(not_visited)
-
+                actual = self.minimum(not_visited) # esta es la original
+                #actual = self.maximum(not_visited)
         else:
             return False
 
+    def reset_visited(self):
+         for i in self.apex:
+             self.apex[i].visited = False
 
 
 
 def main():
     hp = Graph()
 
+    hp.add_apex(0)
     hp.add_apex(1)
     hp.add_apex(2)
     hp.add_apex(3)
     hp.add_apex(4)
-    hp.add_apex(5)
-    hp.add_apex(6)
 
-    hp.add_edge(1, 6, 14)
-    hp.add_edge(1, 2, 7)
-    hp.add_edge(1, 3, 9)
-    hp.add_edge(2, 3, 10)
-    hp.add_edge(2, 4, 15)
-    hp.add_edge(3, 4, 11)
-    hp.add_edge(3, 6, 2)
-    hp.add_edge(4, 5, 6)
-    hp.add_edge(5, 6, 9)
+    hp.add_edge(0, 1, 3)
+    hp.add_edge(0, 3, 7)
+    hp.add_edge(0, 4, 8)
+
+    hp.add_edge(1, 2, 1)
+    hp.add_edge(1, 3, 4)
+
+    hp.add_edge(3, 2, 2)
+
+    hp.add_edge(4, 3, 3)
+
+
 
     print("\nThe faster way for dijkstra with the cost is: ")
-    hp.dijkstra(1)
-    print(hp.path(1, 6))
+
+    hp.dijkstra(0)
+    print(hp.path(0, 4))
+
+    # hp.reset_visited()
+    #
+    #
+    #
+    # hp.add_edge(0, 2, 1)
+    # hp.add_edge(2, 4, 1)
+    #
+    # hp.dijkstra(0)
+    # print(hp.path(0, 4))
+
+
     #print("\n The final graph values are: ")
     #hp.print_graph()
 
