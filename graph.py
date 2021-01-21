ca830b1c5a5c27a0e13b4744cf3b488ca02d1b16
Fernando <fercoche369@gmail.com>
2021-01-20 22:34:45 -0600

    Dijkstra run ways

--- graph.py
+++ graph.py
@@ -19,23 +19,18 @@ class Graph:
         if id not in self.apex:
             self.apex[idf] = Vertex(idf)
 
-    def add_edge (self, a, b, p):
+    def add_edge(self, a, b, p):
         if a in self.apex and b in self.apex:
             self.apex[a].add_neighbor(b, p)
-            #self.apex[b].add_neighbor(a, p)
-
-    def print_graph(self):
-        for i in self.apex:
-            print("The distance from de vertex " + str(i) + " es " + str(self.apex[i].distance) + " from " + str(self.apex[i].dad))
+            # self.apex[b].add_neighbor(a, p)
 
     def path(self, a, b):
         path = []
         actual = b
 
-        while actual != None:
-            path.insert(0, actual) #a por 0
+        while actual is not None:
+            path.insert(0, actual)
             actual = self.apex[actual].dad
-
         return [path, self.apex[b].distance]
 
     def minimum(self, array):
@@ -47,7 +42,6 @@ class Graph:
                 if m > self.apex[i].distance:
                     m = self.apex[i].distance
                     v = i
-
             return v
 
     def maximum(self, array):
@@ -60,9 +54,8 @@ class Graph:
                     v = i
             return v
 
-    def dijkstra1(self, a):
+    def dijkstra_min(self, a):
         if a in self.apex:
-
             self.apex[a].distance = 0
             actual = a
             not_visited = []
@@ -75,19 +68,18 @@ class Graph:
 
             while len(not_visited) > 0:
                 for i in self.apex[actual].neighbor:
-                    if self.apex[i[0]].visited == False:
-                        if self.apex[actual].distance + i[1] <= self.apex[i[0]].distance: # el signo original es "<"
+                    if not self.apex[i[0]].visited:
+                        if self.apex[actual].distance + i[1] <= self.apex[i[0]].distance:
                             self.apex[i[0]].distance = (self.apex[actual].distance + i[1])
                             self.apex[i[0]].dad = actual
 
                 self.apex[actual].visited = True
                 not_visited.remove(actual)
-                actual = self.minimum(not_visited) # esta es la original
-                #actual = self.maximum(not_visited)
+                actual = self.minimum(not_visited)
         else:
             return False
 
-    def dijkstra2(self, a):
+    def dijkstra_max(self, a):
         if a in self.apex:
 
             self.apex[a].distance = 0
@@ -96,54 +88,49 @@ class Graph:
 
             for v in self.apex:
                 if v != a:
-                    self.apex[v].distance = float('inf')
+                    self.apex[v].distance = 0
                 self.apex[v].dad = None
                 not_visited.append(v)
 
             while len(not_visited) > 0:
                 for i in self.apex[actual].neighbor:
-                    if self.apex[i[0]].visited == False:
-                        if (self.apex[actual].distance + i[1] < self.apex[i[0]].distance): # el signo original es "<"
+                    if not self.apex[i[0]].visited:
+                        if self.apex[actual].distance + i[1] > self.apex[i[0]].distance:
                             self.apex[i[0]].distance = (self.apex[actual].distance + i[1])
                             self.apex[i[0]].dad = actual
 
                 self.apex[actual].visited = True
                 not_visited.remove(actual)
-                #actual = self.minimum(not_visited) # esta es la original
                 actual = self.maximum(not_visited)
         else:
             return False
 
     def reset_visited(self):
-         for i in self.apex:
-             self.apex[i].visited = False
+        for i in self.apex:
+            self.apex[i].visited = False
 
+    def get_ways(self, a, b):
+        if a != b:
+            self.dijkstra_min(a)
+            print(self.path(a, b))
+
+            self.reset_visited()
+
+            self.dijkstra_max(a)
+            print(self.path(a, b))
+
+        else:
+            print("Same node")
 
 
 def main():
     hp = Graph()
 
-    # hp.add_apex(0)
-    # hp.add_apex(1)
-    # hp.add_apex(2)
-    # hp.add_apex(3)
-    # hp.add_apex(4)
-    #
-    # hp.add_edge(0, 1, 3)
-    # hp.add_edge(0, 3, 7)
-    # hp.add_edge(0, 4, 8)
-    #
-    # hp.add_edge(1, 2, 1)
-    # hp.add_edge(1, 3, 4)
-    #
-    # hp.add_edge(3, 2, 2)
-    #
-    # hp.add_edge(4, 3, 3)
-
     hp.add_apex(1)
     hp.add_apex(2)
     hp.add_apex(3)
     hp.add_apex(4)
+    hp.add_apex(9)
 
     hp.add_edge(1, 2, 3)
     hp.add_edge(1, 4, 8)
@@ -152,33 +139,9 @@ def main():
     hp.add_edge(2, 4, 3)
     hp.add_edge(3, 4, 8)
 
-    print("\nThe faster way for dijkstra with the cost is: ")
+    hp.add_edge(4, 9, 3)
 
-    hp.dijkstra2(1)
-    print(hp.path(1, 4))
-
-    hp.reset_visited()
-
-    hp.dijkstra1(1)
-    print(hp.path(1, 4))
-
-
-
-
-    #
-    #
-    #
-    # hp.add_edge(0, 2, 1)
-    # hp.add_edge(2, 4, 1)
-    #
-    # hp.dijkstra(0)
-    # print(hp.path(0, 4))
-
-
-    #print("\n The final graph values are: ")
-    #hp.print_graph()
-
-
+    hp.get_ways(1, 4)
 
 
 main()
